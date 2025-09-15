28f2a0f9caa45c69ee98cfc47673dad409f5d8db
Patrick Klaus <patrick.klaus@students.uni-mannheim.de>
2025-09-14 20:02:12 -0500

    First Steps

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,37 @@
+import supabase from "../config/supabaseClient"
+import { useEffect, useState } from "react"
 const Home = () => {
+  const [fetchError, setFetchError] = useState(null)
+  const[directExportData, setDirectExportData] = useState(null)
+
+  useEffect(() => {
+    const fetchDirectExportData = async () => {
+      const { data, error } = await supabase
+        .from('Direct Export')
+        .select('*')
+      if (error) {
+        setFetchError("Could not fetch the data")
+        setDirectExportData(null)
+        console.log(error)
+      } else {
+        setDirectExportData(data)
+        setFetchError(null)
+      }
+    }
+    fetchDirectExportData()
+  }, [])
+
   return (
     <div className="page home">
-      <h2>Home</h2>
-    </div>
+      {fetchError && (<p>{fetchError}</p>)}
+      {directExportData && (
+        <ul>
+          {directExportData.map(item => (
+            <li key={item.id}>{item.created_at}</li>
+          ))}
+        </ul>
+      )}
+    </div> 
   )
 }
 
